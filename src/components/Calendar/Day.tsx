f2d394b5c9b80e1c1e9ca6b655b1a559f0f735a1
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-01-31 06:31:18 -0800

    Change styles of ScreeningPreview component

--- src/components/Calendar/Day.tsx
+++ src/components/Calendar/Day.tsx
@@ -7,7 +7,7 @@ import { getClassesForDayContainer, getDayOfTheMonth, isSameMonth } from "./util
 const Day = ({ day, screenings, monthStart }: { day: Date, screenings: Array<Screening>, monthStart: Date }) => {
 	const dayOfTheMonth = getDayOfTheMonth(day);
 	return (
-		<div className={`border border-gray-200 pt-2 px-5 pb-5 md:flex-1 ${getClassesForDayContainer(day, monthStart)}`}>
+		<div className={`border border-gray-200 pt-2 px-3 pb-5 md:flex-1 ${getClassesForDayContainer(day, monthStart)}`}>
 			{
 				isSameMonth(day, monthStart) ?
 				<Link to={`/showtimes/${dayOfTheMonth}`}>
--- src/components/Calendar/ScreeningPreview.tsx
+++ src/components/Calendar/ScreeningPreview.tsx
@@ -3,8 +3,9 @@ import { Screening } from "../../types";
 import { getClassesForScreeningPreview } from "./utils";
 
 const ScreeningPreview = ({ screening, day }: { screening: Screening, day: Date }) => (
-	<div className={`text-sm mb-2 lg:text-xs ${getClassesForScreeningPreview(day)}`}>
-		{screening.title.toUpperCase()} | {screening.time}
+	<div className={`text-sm text-left mb-2  md:mb-3 lg:text-xs ${getClassesForScreeningPreview(day)}`}>
+		<span className="block font-bold mb-px">{screening.time}</span>
+		<span className="block">{screening.title.toUpperCase()}</span>
 	</div>
 );
 
