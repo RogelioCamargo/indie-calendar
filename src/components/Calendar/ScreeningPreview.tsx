7472a9ec04fddcac27f25c078588ff83806c04db
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-01-31 12:32:37 -0800

    Remove unncessary console logs

--- src/components/Calendar/ScreeningPreview.tsx
+++ src/components/Calendar/ScreeningPreview.tsx
@@ -6,7 +6,6 @@ import { HashLink } from "react-router-hash-link";
 const ScreeningPreview = ({ screening, day }: { screening: Screening, day: Date }) => (
 	<div className={`text-sm text-left mb-2 md:mb-3 lg:text-xs ${getClassesForScreeningPreview(day)}`}>
 		<HashLink to={`/showtimes/${getDayOfTheMonth(day)}#${screening.id}`}>
-			{console.log(`/showtimes/${getDayOfTheMonth(day)}#${screening.id}`)}
 			<span className="block font-bold mb-px">{screening.time}</span>
 			<span className="block">{screening.title.toUpperCase()}</span>
 		</HashLink>
